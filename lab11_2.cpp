49165a9c84a9c66da550c27e0562428ca3db7132
suwichada <jippee.1211@gmail.com>
2021-12-17 15:00:55 +0700

    lab11_2.cpp

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,11 +6,16 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string t;
+	dest<<"-------------------- BOOM ---------------------"<<'\n';
+	while (getline(source,t))
+	{
+		dest<<t<<'\n';
+	}
+	dest<<"-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
